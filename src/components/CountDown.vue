beffd1590cf30db1c7e6c59e5bc1430ee5470849
Stanislav <102070985+NegliadoV@users.noreply.github.com>
2024-07-26 17:19:01 +0300

    Update CountDown.vue

--- src/components/CountDown.vue
+++ src/components/CountDown.vue
@@ -27,8 +27,8 @@ const onCountDown = () => {
   if (remains <= 0) {
     clearInterval(remains);
   }
-  days.value = Math.floor((remains / (1000 * 60 * 60 * 30)) % 24);
-  hours.value = Math.floor((remains / (1000 * 60 * 60)) % 24);
+  days.value = remains 
+hours.value = Math.floor((remains / (1000 * 60 * 60)) % 24);
   minutes.value = Math.floor((remains / (1000 * 60)) % 60);
   seconds.value = Math.floor((remains / 1000) % 60);
 
